46114e8584fa1c898e3699abc5fd5508426488d5
Alex Kollár <alex.kollar@medicover.hu>
2022-03-22 14:34:21 +0100

    Added gitignore, offline test "account" and Texts class to get const text data.

---
+++ Multiboard/MultiboardClient/Utils/Texts.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiboardClient.Utils
+{
+    public class Texts
+    {
+        public const string OFFLINE_TEST_USERNAME = "fronttest";
+        public const string OFFLINE_TEST_PASSWORD = "test123";
+    }
+}
---
+++ Multiboard/MultiboardClient/ViewModels/AuthenticationWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using MultiboardClient.Utils;
+
+namespace MultiboardClient.ViewModels
+{
+    public class AuthenticationWindowViewModel
+    {
+        public async Task<bool> Login(string username, string password)
+        {
+            if (username == Texts.OFFLINE_TEST_USERNAME && password == Texts.OFFLINE_TEST_PASSWORD)
+                return true;
+
+            return false;
+        }
+    }
+}
