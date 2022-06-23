44f02d372b7d912173503a7fea65e911febe63b8
Alex Kollár <alex.kollar@medicover.hu>
2022-06-23 09:17:20 +0200

    Progress commit

--- Multiboard/MultiboardClient/obj/Debug/net6.0-windows/Windows/AuthenticationWindow.g.i.cs
+++ Multiboard/MultiboardClient/obj/Debug/net6.0-windows/Windows/AuthenticationWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace MultiboardClient.Windows {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MultiboardClient;V1.0.0.0;component/windows/authenticationwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MultiboardClient;component/windows/authenticationwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Windows\AuthenticationWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
